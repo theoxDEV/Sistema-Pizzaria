847d108794f13d999b6b3887a9cbb53f10e33bd2
Matheus Nascimento <Matheus.Nascimento1999@gmail.com>
2020-06-26 10:59:12 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# -Pizzaria
+ Sistema para pizzaria/restaurantes
---
+++ index.php
@@ -0,0 +1,71 @@
+<!doctype html>
+<html lang="pt-br">
+  <head>
+    <!-- CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <style type="text/css">
+      a:link
+      {
+        text-decoration:none;
+        color:black;
+      }
+
+      DIV.logo {
+        min-height: 10em;
+        vertical-align: middle;
+        justify-content: center;
+        text-align: center;
+        margin: 0px auto;
+      }
+
+      DIV.headerPageMatheus{
+        bottom: 0;
+        text-align: center;
+        position: absolute;
+        width:100%;
+      }
+    </style>
+
+    <title>Pizzaria!</title>
+  </head>
+  <body>
+
+  <!-- CONTATOS (ROXO) -->
+  <div class="headerPageMatheus">
+    <nav class="container-fluid" style="background-color: #d3d3d3;">
+      <a href="https://www.linkedin.com/in/matheus-nascimento-3582a5175/">
+        <img src="imagens/linkedin.png" width="30" height="30" alt="linkedinIcone">
+        Linkedin
+      </a>
+      <a href="https://github.com/theoxDEV">
+        <img src="imagens/github.png" width="30" height="30" alt="gitHubIcone">
+        GitHub
+      </a>
+    </nav>
+  </div>
+
+    <!-- LOGO/MENU -->
+    <div class='logo'>
+      <img src="imagens/logo.png" width="200" height="200">
+      <div>
+        <h2>
+          PIZZARIA  
+        </h2>
+      </div>
+      </a>
+    </div>
+
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+  </body>
+</html>
